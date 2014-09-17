f3430f258f740e691420e2b130264f03a250d0b2
chaiwat <mynameistecs51@gmail.com>
2014-09-17 14:32:25 +0700

    chaiwat create work

---
+++ application/views/footter.php
@@ -0,0 +1,29 @@
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="<?php echo base_url().'js/jquery-1.11.0.js';?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url().'js/bootstrap.min.js';?>"></script>
+
+</body>
+
+</html>
