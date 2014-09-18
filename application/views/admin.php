8b3e55477ec96cda2e7883e0e8002265481cbe2a
te <te.chaiwat@gmail.com>
2014-09-18 15:51:46 +0700

    chaiwat update

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,81 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php echo $this->load->view('head');?>
+<div class="row show-grid">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Network Service & Solution</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="<?php echo base_url().'css/bootstrap.min.css';?>" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="<?php echo base_url().'css/shop-homepage.css';?>" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Network Service & Solution</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <?php echo anchor('service_con','หน้าหลัก');?>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-
-<div class="col-md-3">
-	<p class="lead">ตั้งค่า</p>
-	<div class="list-group">
-		<a href="#" class="list-group-item">Category 1</a>
-		<a href="#" class="list-group-item">Category 2</a>
-		<a href="#" class="list-group-item">Category 3</a>
-	</div>
+    <div class="col-md-8 col-md-offset-2">
+        <table width="80%" border ="black" align="center ">
+            <tr>
+                <th>
+                    <div class="panel panel-primary">
+                        <?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
+                            <img src=" '.base_url().'image/pic_admin/network_admin.gif"  style="width:200px; hight:150px; padding:10px;"> 
+                        </div>
+                        <div class="panel-footer" align="center">ตั้งค่า network_admin</div>
+                        ');?>                           
+                    </div>
+                </th>
+                <th>
+                    <div class="panel panel-primary">
+                        <?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
+                            <img src=" '.base_url().'image/pic_admin/network.jpg"  style="width:200px; hight:150px; padding:10px;"> 
+                        </div>
+                        <div class="panel-footer" align="center">ตั้งค่า network</div>
+                        ');?>       
+                    </div>
+                </th>
+                <th>
+                    <div class="panel panel-primary">
+                        <?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
+                            <img src=" '.base_url().'image/pic_admin/cctv.jpg"  style="width:200px; hight:150px; padding:10px;"> 
+                        </div>
+                        <div class="panel-footer" align="center">ตั้งค่า cctv</div>
+                        ');?>       
+                    </div>
+                </th>
+            </tr>
+        </table>
+    </div>
 </div>
-
-<div class="col-md-9">
-
-xxxxxxxxxxxxxxxxxx
+----------------------
+<div class="row show-grid col-md-offset-2" >
+    <div class="col-xs-5 col-md-3 " >  
+        <div class="panel panel-primary " centered>
+            <?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
+                <img src=" '.base_url().'image/pic_admin/cctv.jpg"  style="width:200px; hight:150px; position:static;"> 
+            </div>
+            <div class="panel-footer" align="center">ตั้งค่า cctv</div>
+            ');?>       
+        </div> 
+    </div>
+    <div class="col-xs-6 col-md-3 " align="center">  
+        <a href="#" class="thumbnail">
+            <img data-src="holder.js/100%x180" alt="...">
+        </a>
+    </div>
+    <div class="col-xs-6 col-md-3 " align="center">
+        <a href="#" class="thumbnail">
+            <img data-src="holder.js/100%x180" alt="...">
+        </a>
+    </div>
 </div>
-
 <?php $this->load->view('footter');?>
