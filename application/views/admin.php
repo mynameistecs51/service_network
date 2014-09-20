b89e6bd2bf087acef75d628ae66e4bfefd6c7503
tete <mynameistecs51@gmail.com>
2014-09-20 12:22:38 +0700

    chaiwat update admin_con.php & delete picture network_admin.gif

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,37 +1,44 @@
 <?php echo $this->load->view('head');?>
 
 <div class="row " >
+	<h3>Configuration <span class="glyphicon glyphicon-wrench"></span></h3><br/><br/>
 	<div class="col-xs-5 col-md-3 " align="center">  
 		<div class="panel panel-primary " >
 			<?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
-				<img src=" '.base_url().'image/pic_admin/network_admin.gif"  style="width:200px; hight:150px;  position:relative;  
-				margin:auto;  
-				display:block; "> 
+				<img src=" '.base_url().'image/pic_admin/network_admin.jpg" style="width:200px; height:150px;">
 			</div>
-			<div class="panel-footer" align="center">ตั้งค่า network admin cctv</div>
+			<div class="panel-footer" align="center"><b> Network Admin </b></div>
 			');?>       
 		</div> 
 	</div>
 	<div class="col-xs-6 col-md-3 " align="center">  
 		<div class="panel panel-primary " >
 			<?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
-				<img src=" '.base_url().'image/pic_admin/network.jpg"  style="width:200px; hight:150px;  position:relative;  
-				margin:auto;  
-				display:block; "> 
+				<img src=" '.base_url().'image/pic_admin/network.jpg"  style="width:200px; height:150px;"> 
 			</div>
-			<div class="panel-footer" align="center">ตั้งค่า cctv</div>
+			<div class="panel-footer" align="center"><b> Network  </b></div>
 			');?>     
 		</div>
 	</div>
 	<div class="col-xs-6 col-md-3 " align="center">
 		<div class="panel panel-primary " >
 			<?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
-				<img src=" '.base_url().'image/pic_admin/cctv.jpg"  style="width:200px; hight:150px; "> 
+				<img src=" '.base_url().'image/pic_admin/cctv.jpg"  style="width:200px; height:150px; "> 
 			</div>
-			<div class="panel-footer" align="center">ตั้งค่า cctv</div>
+			<div class="panel-footer" align="center"><b> CCTV </b></div>
 			');?>     
 		</div>
 	</div>
+	<div class="col-xs-6 col-md-3 " align="center">  
+		<div class="panel panel-primary " >
+			<?php echo anchor('ctl_main/add_teacher','<div class="panel-body" >
+				<img src=" '.base_url().'image/pic_admin/Bee-Customized-icon.jpg"  style="width:200px; height:150px; "> 
+			</div>
+			<div class="panel-footer" align="center"><b> ผลงานที่ผ่านมา  </b></div>
+			');?>     
+		</div>
+	</div>
+
 </div>
 
 <?php $this->load->view('footter');?>
