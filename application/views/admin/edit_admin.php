f9c1831dfb591490bcac99717092687fbd440d32
chaiwat <mynameistecs51@gmail.com>
2014-09-21 16:31:27 +0700

    chaiwt update

--- application/views/admin/edit_admin.php
+++ application/views/admin/edit_admin.php
@@ -25,16 +25,17 @@
 	<div class="panel panel-primary">
 		<div class="panel-heading">
 			<h3><?php echo "Config ".$page;?> <span class="glyphicon glyphicon-wrench"></span></h3>
-		</div>
+		</div>	<!-- ---------------- end div class="panel-heading"---------------------- -->
 		<div class="panel-body">
 			<ul class="nav nav-tabs" role="tablist">
 				<li class="active"><a href="#">เพิ่ม</a></li>
 				<li><a href="#">ลบ</a></li>
 				<li><a href="#">แกไข</a></li>
 				<li><a href="#">ค้นหา</a></li>
-			</ul>
-			<br/>
+			</ul><br/>
+			
 			<div class="row col-md-offset-2">
+
 				<?php echo form_open_multipart('admin_con/add_detail',' class="form-horizontal" role="form" ');?>
 
 				<div class="form-group col-xs-6 col-xs-6 .col-md-4">
@@ -48,79 +49,78 @@
 					<div class="col-sm-8">
 						<select class="form-control" id="input_group" name="input_group" readonly>
 							<?php
-							$group_detail_id ; # group_id
+								$group_detail_id ; // group_id
 
-							foreach ($show_group as $group => $service_group) {
-								# code...	select group ....
-								
-								if($service_group->group_name == $page){		
-								$show_group_detail = $service_group->group_id; #ประกาศให้ใช้ร่วมกัน
+								foreach ($show_group as $group => $service_group) {
+									// code...	select group ....
 
-								echo '<option value="'.$service_group->group_id.'">'.$service_group->group_name.'</option>';
+									if($service_group->group_name == $page){		
+										$show_group_detail = $service_group->group_id; //ประกาศให้ใช้ร่วมกัน
+
+										echo '<option value="'.$service_group->group_id.'">'.$service_group->group_name.'</option>';
+									}
+								}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="form-group col-xs-6">
+						<label for="input_detail" class="col-sm-2 control-label">detail	</label>
+						<div class="col-sm-8">
+							<textarea class="form-control" rows="3" id="input_detail" name="input_detail"></textarea>
+						</div>
+					</div>
+					<div class="form-group col-xs-6">
+						<label for="input_picture" class="col-sm-2 control-label">picture	</label>
+						<div class="col-sm-8">
+							<img id="show_pic" src="<?php echo base_url().'image/pic_admin/no-image.jpg';?>" style="width:130px; height:70px" /><br/>
+							<input id="input_picture" class="form-control" type="file" name="input_picture" onchange="PreviewImage();" multiple/>
+						</div>
+					</div>
+
+					<div class="col-sm-offset-8  col-xs-4">
+						<button type="reset" class="btn btn-default" value="reset">reset</button>
+						<button type="submit" class="btn btn-default" value="save">save</button>
+					</div>
+				</form>
+			</div>  <!-- --- end class="row col-md-offset-2" --------- -->
+			<hr>
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>คำอธิบาย</th>
+						<th>ภาพ</th>
+						<th>group</th>
+						<th>edit delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					foreach ($query_service_by_group as $key => $service_group_value) {
+						echo "<tr>";
+						echo "<td>";
+						echo "#";
+						echo "</td>";
+						echo "<td>";
+								echo $service_group_value->detail_text;		//โชว์ รายละเอียด
+								echo "</td>";
+								echo "<td>";	
+								echo $service_group_value->pic_name;			///โชว์รูปภาพ
+								echo "</td>";
+								echo "<td>";
+								echo $service_group_value->group_name;		//โชว์กลุ่ม
+								echo "</td>";
+								echo "<td>";
+								echo "EDIT  DELETE";
+								echo "</td>";
+								echo "</tr>";
 							}
-						}
-						?>
-					</select>
+							?>
+						</tbody>
+					</table>	
 				</div>
-			</div>
-			<div class="form-group col-xs-6">
-				<label for="input_detail" class="col-sm-2 control-label">detail	</label>
-				<div class="col-sm-8">
-					<textarea class="form-control" rows="3" id="input_detail" name="input_detail"></textarea>
-				</div>
-			</div>
-			<div class="form-group col-xs-6">
-				<label for="input_picture" class="col-sm-2 control-label">picture	</label>
-				<div class="col-sm-8">
-					<img id="show_pic" src="<?php echo base_url().'image/pic_admin/no-image.jpg';?>" style="width:130px; height:70px" /><br/>
-					<input id="input_picture" class="form-control" type="file" name="input_picture" onchange="PreviewImage();" multiple/>
-				</div>
-			</div>
-
-			<div class="col-sm-offset-8  col-xs-4">
-				<button type="reset" class="btn btn-default" value="reset">reset</button>
-				<button type="submit" class="btn btn-default" value="save">save</button>
 			</div>
 		</div>
-	</form>
-	<hr>
-	<table class="table table-hover">
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>คำอธิบาย</th>
-				<th>ภาพ</th>
-				<th>group</th>
-				<th>edit delete</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			foreach ($query_service_by_group as $key => $service_group_value) {
-				# code...
-				echo "<tr>";
-				echo "<td>";
-				echo "#";
-				echo "</td>";
-				echo "<td>";
-				echo $service_group_value->detail_text;
-				echo "</td>";
-				echo "<td>";
-				echo $service_group_value->pic_name;
-				echo "</td>";
-				echo "<td>";
-				echo $service_group_value->group_name;
-				echo "</td>";
-				echo "<td>";
-				echo "EDIT  DELETE";
-				echo "</td>";
-				echo "</tr>";
-			}
-			?>
-		</tbody>
-	</table>	
-</div>
-</div>
-</div>
-</div>
-<?php $this->load->view('footter');?>
+	</div>
+	<?php $this->load->view('footter');?>
